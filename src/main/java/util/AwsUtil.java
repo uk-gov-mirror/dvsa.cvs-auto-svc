2186e122d4710bcd6ab743c2b1d631df50095959
bflorea <bflorea@deloittece.com>
2020-04-30 16:48:55 +0300

    CloudWatch test with retry

--- src/main/java/util/AwsUtil.java
+++ src/main/java/util/AwsUtil.java
@@ -513,7 +513,7 @@ public class AwsUtil {
                 }
             }
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
