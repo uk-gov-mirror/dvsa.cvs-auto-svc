2dcc267749b5564ef5d09e125ac15ba0fdc6fa39
bflorea <bflorea@deloittece.com>
2020-04-30 15:58:10 +0300

    CloudWatch test with retry

--- src/test/java/activities/InsertActivitiesPutCloudWatchLogs.java
+++ src/test/java/activities/InsertActivitiesPutCloudWatchLogs.java
@@ -31,7 +31,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode202() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = UUID.randomUUID().toString();
         // generate random TesterStaffId
@@ -39,12 +39,8 @@ public class InsertActivitiesPutCloudWatchLogs {
 
         DateTime currentTimestamp = DateTime.now().withZone(DateTimeZone.UTC);
         DateTime startTimestamp = currentTimestamp;
-        DateTime endTimestamp = currentTimestamp.plusHours(1);
         String startTime = startTimestamp.toInstant().toString();
-        String endTime = endTimestamp.toInstant().toString();
-
         String startTimeGet = startTimestamp.minusSeconds(1).toInstant().toString();
-        String endTimeGet = endTimestamp.plusSeconds(1).toInstant().toString();
 
         // generate endTime
 
@@ -53,18 +49,16 @@ public class InsertActivitiesPutCloudWatchLogs {
         JsonPathAlteration alterationTesterStaffId = new JsonPathAlteration("$.testerStaffId",
                 randomTesterStaffId,"","REPLACE");
         JsonPathAlteration alterationStartTime = new JsonPathAlteration("$.startTime",startTime ,"","REPLACE");
-        JsonPathAlteration alterationEndTime = new JsonPathAlteration("$.endTime", endTime,"","REPLACE");
 
         // initialize the alterations list with both declared alteration
         List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
                 alterationId,
                 alterationStartTime,
-                alterationEndTime,
                 alterationTesterStaffId));
 
 
         activitiesSteps.insertActivityWithAlterations(postRequestBody, alterations);
-        activitiesSteps.getActivities("visit", randomTesterStaffId, null, startTimeGet, endTimeGet);
+        activitiesSteps.getActivities("visit", randomTesterStaffId, null, startTimeGet , null);
         activitiesSteps.statusCodeShouldBe(200);
 
         activitiesSteps.putActivitiesEnd(randomId);
@@ -82,7 +76,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode400() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404400-7e57-c0de-e400-e4404c0de400";
         // generate random TesterStaffId
@@ -129,7 +123,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode401() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404401-7e57-c0de-e401-e4404c0de401";
         // generate random TesterStaffId
@@ -176,7 +170,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode403() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404403-7e57-c0de-e403-e4404c0de403";
         // generate random TesterStaffId
@@ -224,7 +218,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode404() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404404-7e57-c0de-e404-e4404c0de404";
         // generate random TesterStaffId
@@ -271,7 +265,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode429() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404429-7e57-c0de-e429-e4404c0de429";
         // generate random TesterStaffId
@@ -318,7 +312,7 @@ public class InsertActivitiesPutCloudWatchLogs {
     @Test
     public void insertPutActivityVisitHttpCode500() {
         // read post request body from file
-        String postRequestBody = GenericData.readJsonValueFromFile("activities_10767.json","$");
+        String postRequestBody = GenericData.readJsonValueFromFile("activities_10769.json","$");
         // generate random ID
         String randomId = "e4404500-7e57-c0de-e500-e4404c0de500";
         // generate random TesterStaffId
