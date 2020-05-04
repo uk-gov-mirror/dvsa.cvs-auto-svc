e03eda419656efa645e7049f69c3fd9baa9a4795
bflorea <bflorea@deloittece.com>
2020-05-04 18:08:22 +0300

    test-results and activities tests ready

--- src/test/java/activities/PostActivitiesCloudWatchLogs.java
+++ src/test/java/activities/PostActivitiesCloudWatchLogs.java
@@ -213,7 +213,7 @@ public class PostActivitiesCloudWatchLogs {
 
     }
 
-     @WithTag("In_Test")
+//     @WithTag("In_Test")
     @Title("CVSB-10767 CVS to EDH (Open Site Visits) POST - AC1 - http status code 404")
     @Test
     public void insertPostActivityVisitHttpCode404() {
@@ -259,7 +259,7 @@ public class PostActivitiesCloudWatchLogs {
 
     }
 
-//    @WithTag("In_Test")
+    @WithTag("In_Test")
     @Title("CVSB-10767 CVS to EDH (Open Site Visits) POST - AC1 - http status code 429")
     @Test
     public void insertPostActivityVisitHttpCode429() {
