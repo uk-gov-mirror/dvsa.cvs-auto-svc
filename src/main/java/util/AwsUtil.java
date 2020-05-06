98bf8970d2c7f81f753de790f30d22e730358fae
bflorea <bflorea@deloittece.com>
2020-05-06 11:53:29 +0300

    test-results and activities tests ready

--- src/main/java/util/AwsUtil.java
+++ src/main/java/util/AwsUtil.java
@@ -477,13 +477,14 @@ public class AwsUtil {
         String logGroup = "/aws/lambda/edh-dispatcher-" + System.getProperty("BRANCH");
 
         logStreamLoop:
-        for (int times = 0; times < 20; times++) {
+        for (int times = 0; times < 40; times++) {
             System.out.println("... " + times + " ...");
             DescribeLogStreamsRequest describeLogStreamsRequest = new DescribeLogStreamsRequest()
                     .withLogGroupName(logGroup)
                     .withOrderBy("LastEventTime")
                     .withDescending(true)
-                    .withLimit(30);
+//                    .withLimit(50)
+                    ;
 
             DescribeLogStreamsResult describeLogStreamsResult = logsClient.describeLogStreams(describeLogStreamsRequest);
 
