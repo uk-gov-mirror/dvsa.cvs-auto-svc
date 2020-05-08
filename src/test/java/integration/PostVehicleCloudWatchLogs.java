8cb3bbbbd67c6af53e8a17f62e1eea967d920744
bflorea <bflorea@deloittece.com>
2020-05-08 20:25:46 +0300

    test-results and activities tests ready

--- src/test/java/integration/PostVehicleCloudWatchLogs.java
+++ src/test/java/integration/PostVehicleCloudWatchLogs.java
@@ -63,7 +63,7 @@ public class PostVehicleCloudWatchLogs {
     }
 
 //    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 400")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC2 - AC2 - POST request is made and EDH responds back with HTTP Error code 400")
     @Test
     public void testVehiclePostHttpCode400() {
         // TEST SETUP
@@ -96,7 +96,7 @@ public class PostVehicleCloudWatchLogs {
     }
 
 //    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 401")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC2 - AC2 - POST request is made and EDH responds back with HTTP Error code 401")
     @Test
     public void testVehiclePostHttpCode401() {
         // TEST SETUP
@@ -129,7 +129,7 @@ public class PostVehicleCloudWatchLogs {
     }
 
 //    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 403")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC2 - AC2 - POST request is made and EDH responds back with HTTP Error code 403")
     @Test
     public void testVehiclePostHttpCode403() {
         // TEST SETUP
@@ -162,7 +162,7 @@ public class PostVehicleCloudWatchLogs {
     }
 
 //    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 404")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC2 - AC2 - POST request is made and EDH responds back with HTTP Error code 404")
     @Test
     public void testVehiclePostHttpCode404() {
         // TEST SETUP
@@ -195,7 +195,7 @@ public class PostVehicleCloudWatchLogs {
     }
 
 //    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 429")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC3 - AC3 - POST request is made and EDH responds back with HTTP Error code 429")
     @Test
     public void testVehiclePostHttpCode429() {
         // TEST SETUP
@@ -227,8 +227,8 @@ public class PostVehicleCloudWatchLogs {
         vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
     }
 
-    @WithTag("In_Test")
-    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 500")
+//    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC3 - AC3 - POST request is made and EDH responds back with HTTP Error code 500")
     @Test
     public void testVehiclePostHttpCode500() {
         // TEST SETUP
@@ -260,4 +260,45 @@ public class PostVehicleCloudWatchLogs {
         vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
     }
 
+    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - PUT request is made and EDH responds back with HTTP 202 Accepted status")
+    @Test
+    public void testVehiclePutHttpCode202() {
+        // TEST SETUP
+        // generate random systemNumber
+        String randomSystemNumber = AwsUtil.getNextSystemNumberInSequence();
+        // generate random Vin
+        String randomVin = GenericData.generateRandomVin();
+        // generate random Vrm
+        String randomVrm = GenericData.generateRandomVrm();
+
+        // read post request body from file
+        String requestBody = GenericData.readJsonValueFromFile("technical-records_integration_hgv_10775.json", "$");
+
+        // create alterations to change attributes
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrm = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+
+        // initialize the alterations list
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                alterationVin,
+                alterationVrm,
+                alterationSystemNumber
+        ));
+
+        //TEST
+        vehicleTechnicalRecordsSteps.postVehicleTechnicalRecordsWithAlterations(requestBody, alterations);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(201);
+        vehicleTechnicalRecordsSteps.getVehicleTechnicalRecords(randomSystemNumber);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(200);
+        vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("techRecord.size()", 1);
+        vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("[0].systemNumber", randomSystemNumber);
+        // read the adr details from the file used for put request body with battery adr details
+        String adrDetails = GenericData.readJsonValueFromFile("technical-records_adr_details_tank_nulls.json", "$.techRecord[0].adrDetails");
+        JsonPathAlteration alterationAddAdrDetails = new JsonPathAlteration("$.techRecord[0]", adrDetails,"adrDetails","ADD_FIELD");
+        alterations.add(alterationAddAdrDetails);
+        vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("PUT", randomSystemNumber, 202);
+        vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
+    }
 }
