f5d0682a5994fa5ffbd62c4b64df82ca1417cb76
bflorea <bflorea@deloittece.com>
2020-05-08 18:47:31 +0300

    test-results and activities tests ready

--- src/test/java/integration/PostVehicleCloudWatchLogs.java
+++ src/test/java/integration/PostVehicleCloudWatchLogs.java
@@ -101,7 +101,7 @@ public class PostVehicleCloudWatchLogs {
     public void testVehiclePostHttpCode401() {
         // TEST SETUP
         // generate random systemNumber
-        String randomSystemNumber = "e4404400-7e57-c0de-e400-e4404c0de400";
+        String randomSystemNumber = "e4404401-7e57-c0de-e401-e4404c0de401";
         // generate random Vin
         String randomVin = GenericData.generateRandomVin();
         // generate random Vrm
@@ -125,6 +125,138 @@ public class PostVehicleCloudWatchLogs {
         //TEST
         vehicleTechnicalRecordsSteps.insertVehicleWithAlterations(requestBody, alterations);
         vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("POST", randomSystemNumber, 401);
+        vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
+    }
+
+//    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 403")
+    @Test
+    public void testVehiclePostHttpCode403() {
+        // TEST SETUP
+        // generate random systemNumber
+        String randomSystemNumber = "e4404403-7e57-c0de-e403-e4404c0de403";
+        // generate random Vin
+        String randomVin = GenericData.generateRandomVin();
+        // generate random Vrm
+        String randomVrm = GenericData.generateRandomVrm();
+
+        // read post request body from file
+        String requestBody = GenericData.readJsonValueFromFile("technical-records_hgv_insert_10775.json", "$");
+
+        // create alterations to change attributes
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrm = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+
+        // initialize the alterations list
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                alterationVin,
+                alterationVrm,
+                alterationSystemNumber
+        ));
+
+        //TEST
+        vehicleTechnicalRecordsSteps.insertVehicleWithAlterations(requestBody, alterations);
+        vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("POST", randomSystemNumber, 403);
+        vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
+    }
+
+//    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 404")
+    @Test
+    public void testVehiclePostHttpCode404() {
+        // TEST SETUP
+        // generate random systemNumber
+        String randomSystemNumber = "e4404404-7e57-c0de-e404-e4404c0de404";
+        // generate random Vin
+        String randomVin = GenericData.generateRandomVin();
+        // generate random Vrm
+        String randomVrm = GenericData.generateRandomVrm();
+
+        // read post request body from file
+        String requestBody = GenericData.readJsonValueFromFile("technical-records_hgv_insert_10775.json", "$");
+
+        // create alterations to change attributes
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrm = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+
+        // initialize the alterations list
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                alterationVin,
+                alterationVrm,
+                alterationSystemNumber
+        ));
+
+        //TEST
+        vehicleTechnicalRecordsSteps.insertVehicleWithAlterations(requestBody, alterations);
+        vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("POST", randomSystemNumber, 404);
+        vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
+    }
+
+//    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 429")
+    @Test
+    public void testVehiclePostHttpCode429() {
+        // TEST SETUP
+        // generate random systemNumber
+        String randomSystemNumber = "e4404429-7e57-c0de-e429-e4404c0de429";
+        // generate random Vin
+        String randomVin = GenericData.generateRandomVin();
+        // generate random Vrm
+        String randomVrm = GenericData.generateRandomVrm();
+
+        // read post request body from file
+        String requestBody = GenericData.readJsonValueFromFile("technical-records_hgv_insert_10775.json", "$");
+
+        // create alterations to change attributes
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrm = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+
+        // initialize the alterations list
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                alterationVin,
+                alterationVrm,
+                alterationSystemNumber
+        ));
+
+        //TEST
+        vehicleTechnicalRecordsSteps.insertVehicleWithAlterations(requestBody, alterations);
+        vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("POST", randomSystemNumber, 429);
+        vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
+    }
+
+//    @WithTag("In_Test")
+    @Title("CVSB-17775 - CVS to EDH (Technical records) - TC1 - AC1 - POST request is made and EDH responds back with HTTP Error code 500")
+    @Test
+    public void testVehiclePostHttpCode500() {
+        // TEST SETUP
+        // generate random systemNumber
+        String randomSystemNumber = "e4404500-7e57-c0de-e500-e4404c0de500";
+        // generate random Vin
+        String randomVin = GenericData.generateRandomVin();
+        // generate random Vrm
+        String randomVrm = GenericData.generateRandomVrm();
+
+        // read post request body from file
+        String requestBody = GenericData.readJsonValueFromFile("technical-records_hgv_insert_10775.json", "$");
+
+        // create alterations to change attributes
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrm = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+
+        // initialize the alterations list
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                alterationVin,
+                alterationVrm,
+                alterationSystemNumber
+        ));
+
+        //TEST
+        vehicleTechnicalRecordsSteps.insertVehicleWithAlterations(requestBody, alterations);
+        vehicleTechnicalRecordsSteps.checkAwsDispatcherLogStatusCodeForSystemNumber("POST", randomSystemNumber, 500);
         vehicleTechnicalRecordsSteps.deleteRecords(randomSystemNumber);
     }
 
