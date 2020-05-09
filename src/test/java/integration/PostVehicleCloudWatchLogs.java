c13393ffa17bd73e1e5f9e0af33a28b167d74f1e
bflorea <bflorea@deloittece.com>
2020-05-09 16:51:08 +0300

    test-results and activities tests ready

--- src/test/java/integration/PostVehicleCloudWatchLogs.java
+++ src/test/java/integration/PostVehicleCloudWatchLogs.java
@@ -355,7 +355,7 @@ public class PostVehicleCloudWatchLogs {
 
     }
 
-    @WithTag("In_Test")
+//    @WithTag("In_Test")
     @Title("CVSB-17775 - CVS to EDH (Technical records) - TC5 - AC5 - PUT request is made and EDH responds back with HTTP Error code 401")
     @Test
     public void testVehiclePutHttpCode401() {
@@ -403,7 +403,7 @@ public class PostVehicleCloudWatchLogs {
 
     }
 
-    //    @WithTag("In_Test")
+    @WithTag("In_Test")
     @Title("CVSB-17775 - CVS to EDH (Technical records) - TC5 - AC5 - PUT request is made and EDH responds back with HTTP Error code 403")
     @Test
     public void testVehiclePutHttpCode403() {
