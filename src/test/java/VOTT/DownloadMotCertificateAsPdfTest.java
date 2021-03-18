ddfed4bf66024ba6e4d65bcc8ac6a4d16d34ab1a
Emanuel State <49722579+estatebjss@users.noreply.github.com>
2021-03-18 11:04:55 +0000

    Added few more tet scenarios

--- src/test/java/VOTT/DownloadMotCertificateAsPdfTest.java
+++ src/test/java/VOTT/DownloadMotCertificateAsPdfTest.java
@@ -6,8 +6,12 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Base64;
 
 public class DownloadMotCertificateAsPdfTest {
@@ -20,7 +24,7 @@ public class DownloadMotCertificateAsPdfTest {
     }
 
     @Test
-    public void DownloadCertificateHappyPath() {
+    public void DownloadTestCertificate() {
 
         RestAssured.baseURI = "https://api.develop.cvs.dvsacloud.uk/cvsb-19156/v1/document-retrieval";
 
@@ -55,10 +59,22 @@ public class DownloadMotCertificateAsPdfTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //Open downloaded pdf file in system default app
+        if (Desktop.isDesktopSupported()) {
+            try {
+                File myFile = new File("src/test/resources/DownloadedMotTestCertificate/TestCert.pdf");
+                Desktop.getDesktop().open(myFile);
+            } catch (FileNotFoundException ex) {
+                System.out.println("File not found" + ex.getMessage());
+            } catch (IOException ex) {
+                System.out.println("No application registered for PDFs" + ex.getMessage());
+            }
+        }
     }
 
     @Test
-    public void DownloadCertificateSadPath() {
+    public void DownloadTestCertificateBadJwtToken() {
 
         RestAssured.baseURI = "https://api.develop.cvs.dvsacloud.uk/cvsb-19156/v1/document-retrieval";
 
@@ -80,6 +96,52 @@ public class DownloadMotCertificateAsPdfTest {
         then().//log().all().
             statusCode(403).
             body("message", equalTo("User is not authorized to access this resource with an explicit deny"));
+    }
+
+    @Test
+    public void DownloadTestCertificateNoVinNumber() {
+
+        RestAssured.baseURI = "https://api.develop.cvs.dvsacloud.uk/cvsb-19156/v1/document-retrieval";
+
+        System.out.println("Valid access token" + token);
+
+        //prep request
+        given()//.log().all()
+                .header("authorization", "Bearer " + token)
+                .header("x-api-key", "YTRasdsadADSDEQ01asdasdasbd67845FDGGDGvww-cvsb-19156")
+                .header("content-type", "application/pdf")
+                .queryParam("testNumber", "W01A00229").
+
+        //send request
+        when().//log().all().
+                get().
+
+        //verification
+        then().//log().all().
+                statusCode(400);
+    }
+
+    @Test
+    public void DownloadTestCertificateNoTestNumber() {
+
+        RestAssured.baseURI = "https://api.develop.cvs.dvsacloud.uk/cvsb-19156/v1/document-retrieval";
+
+        System.out.println("Valid access token" + token);
+
+        //prep request
+        given()//.log().all()
+                .header("authorization", "Bearer " + token)
+                .header("x-api-key", "YTRasdsadADSDEQ01asdasdasbd67845FDGGDGvww-cvsb-19156")
+                .header("content-type", "application/pdf")
+                .queryParam("vinNumber", "T12765432").
+
+        //send request
+        when().//log().all().
+                get().
+
+        //verification
+        then().//log().all().
+                statusCode(400);
     }
 }
 
