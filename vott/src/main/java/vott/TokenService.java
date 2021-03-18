24bb1f2e0fbdc2850ed9ed72ba35d54a78b888be
Emanuel State <49722579+estatebjss@users.noreply.github.com>
2021-03-18 09:54:51 +0000

    Code and comments cleanup

--- vott/src/main/java/vott/TokenService.java
+++ vott/src/main/java/vott/TokenService.java
@@ -6,6 +6,13 @@ import static io.restassured.RestAssured.given;
 import java.time.Duration;
 import java.time.Instant;
 
+/*
+
+TokenService returns a new JWT token when called.
+It also checks if another token was generated recently.
+Returns new token only if previous token expired.
+
+*/
 
 public class TokenService {
 
@@ -33,7 +40,7 @@ public class TokenService {
         RestAssured.baseURI = "https://login.microsoftonline.com/6c448d90-4ca1-4caf-ab59-0a2aa67d7801/oauth2/token";
 
         String response =
-                given()//log().all().
+                given()//.log().all()
                         .formParam("grant_type", "password")
                         .formParam("userName", "cvs.automation1@dvsagov.onmicrosoft.com")
                         .formParam("password", "*7vU4EWuJPwues_nGKBMaaNYVC3434")
