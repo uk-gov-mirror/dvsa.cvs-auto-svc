ac1af5e4e3d82738be55945b203ea6a50065bae3
Robert Whitehouse <Robert.Whitehouse@bjss.com>
2021-04-14 14:24:21 +0100

    merged across database upsert methods, models and repositorys

---
+++ vott/src/main/java/vott/database/connection/DatabaseConfiguration.java
@@ -0,0 +1,48 @@
+package vott.database.connection;
+
+import com.google.gson.Gson;
+import lombok.Builder;
+import lombok.Data;
+import vott.aws.AWSSecrets;
+import vott.aws.Secret;
+import vott.json.GsonInstance;
+
+import java.util.Map;
+
+@Data
+@Builder
+public class DatabaseConfiguration {
+
+    private String host;
+    private int port;
+    private String engine;
+
+    private String username;
+    private String password;
+
+    private String dbName;
+
+    public static DatabaseConfiguration connectionBuilder() {
+
+        Map<String, String> env = System.getenv();
+
+        AWSSecrets secrets = new AWSSecrets();
+        String dbConfig = secrets.getSecret(env);
+
+        Gson gson = GsonInstance.get();
+        Secret authSecret = gson.fromJson(dbConfig, Secret.class);
+
+        return DatabaseConfiguration.builder()
+                .host(authSecret.getHost())
+                .port(authSecret.getPort())
+                .username(authSecret.getUsername())
+                .password(authSecret.getPassword())
+                .dbName(authSecret.getDbName())
+                .engine(authSecret.getEngine())
+                .build();
+    }
+
+    public String toJdbcUrl() {
+        return "jdbc:"+engine+"://" + host + ":" + port + "/" + dbName;
+    }
+}
